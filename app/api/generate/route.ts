ae440ac7bf0a7fe4db887d083bea806e3ea21b7d
Jay Pokale <jay.pokale.35@gmail.com>
2023-11-02 23:21:28 +0530

    Try catch in openai api

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -17,32 +17,38 @@ export async function POST(req: Request): Promise<Response> {
     );
   }
 
-  let { prompt } = await req.json();
+  try {
+    let { prompt } = await req.json();
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are an AI writing assistant that continues existing text based on context from prior text. " +
-          "Give more weight/priority to the later characters than the beginning ones. " +
-          "Limit your response to no more than 200 characters, but make sure to construct complete sentences.",
-      },
-      {
-        role: "user",
-        content: prompt,
-      },
-    ],
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stream: true,
-    n: 1,
-  });
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an AI writing assistant that continues existing text based on context from prior text. " +
+            "Give more weight/priority to the later characters than the beginning ones. " +
+            "Limit your response to no more than 200 characters, but make sure to construct complete sentences.",
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stream: true,
+      n: 1,
+    });
 
-  const stream = OpenAIStream(response);
+    const stream = OpenAIStream(response);
 
-  return new StreamingTextResponse(stream);
+    return new StreamingTextResponse(stream);
+  } catch {
+    return new Response(
+      "There's some problem in your network or API key is expired"
+    );
+  }
 }
