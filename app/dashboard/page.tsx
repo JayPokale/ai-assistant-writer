3660a72fa8fe48e559f23ca300ca768021cc98b2
Jay Pokale <jay.pokale.35@gmail.com>
2023-11-13 18:26:36 +0530

    Shadcn

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,50 @@
-import Link from "next/link";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const Dashboard = () => {
   return (
     <main className="flex-1 flex flex-col gap-4 justify-center items-center">
-      <h1 className="font-semibold text-7xl text-center">
-        AI <span className="text-purple-600 font-bold">assistant</span>{" "}
-      </h1>
       <h2 className="max-w-lg font-semibold text-3xl text-center text-slate-700">
-        Get your AI based writing assistant in your hand.
+        Create New Notebook
       </h2>
-      <Link href="/dashboard" className="flex justify-center mt-4">
+      <p className="text-xl font-bold text-gray-400">or</p>
+      <h2 className="max-w-lg font-semibold text-3xl text-center text-slate-700">
+        View left menu to work on previous notebooks
+      </h2>
+      <div className="flex justify-center mt-4">
         <button className="bg-black hover:bg-transparent text-white hover:text-black duration-200 border-2 border-transparent hover:border-black px-4 py-2 rounded-lg">
           New Notebook
         </button>
-      </Link>
+        {/* <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <button className="bg-black hover:bg-transparent text-white hover:text-black duration-200 border-2 border-transparent hover:border-black px-4 py-2 rounded-lg">
+              New Notebook
+            </button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Write title of your</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog> */}
+      </div>
     </main>
   );
 };
