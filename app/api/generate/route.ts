0228ebf7611d4a52afbb442a857c372c4247863f
Jay Pokale <jay.pokale.35@gmail.com>
2024-01-17 11:38:39 +0530

    Local server added

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,3 +1,4 @@
+// Server 1
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
@@ -48,7 +49,63 @@ export async function POST(req: Request): Promise<Response> {
     return new StreamingTextResponse(stream);
   } catch {
     return new Response(
-      "There's some problem in your network or API key is expired"
+      "There's some error occured while generating api responce"
     );
   }
 }
+
+// Server 2
+/*
+import { StreamingTextResponse } from "ai";
+
+export const runtime = "edge";
+
+export async function POST(): Promise<Response> {
+  const serverUri = process.env.GO_SERVER_URI;
+
+  if (!serverUri || serverUri === "") {
+    return new Response(
+      "Missing SERVER_URI – make sure to add it to your .env file.",
+      {
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    const response = await fetch(serverUri);
+    const text = await response.text();
+    const words = text.split(" ");
+
+    const generateWords = async function* () {
+      for (const word of words) {
+        yield word + " ";
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    };
+
+    const readableStream = new ReadableStream({
+      async start(controller) {
+        const iterator = generateWords()[Symbol.asyncIterator]();
+
+        while (true) {
+          const { done, value } = await iterator.next();
+
+          if (done) {
+            controller.close();
+            break;
+          }
+
+          controller.enqueue(value);
+        }
+      },
+    });
+
+    return new StreamingTextResponse(readableStream);
+  } catch {
+    return new Response(
+      "There's some error occured while generating api responce"
+    );
+  }
+}
+*/
