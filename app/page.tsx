93d71c45bdb2d5b1a66183f9ffc2a280c76d01c8
Jay Pokale <jay.pokale.35@gmail.com>
2024-01-17 13:39:06 +0530

    TITLE CHANGE

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col gap-4 justify-center items-center">
       <h1 className="font-semibold text-7xl text-center">
-        AI <span className="text-purple-600 font-bold">assistant</span>{" "}
+        AI Blog <span className="text-purple-600 font-bold">assistant</span>{" "}
       </h1>
       <h2 className="max-w-lg font-semibold text-3xl text-center text-slate-700">
         Get your AI based writing assistant in your hand.
